cc2603811c60caad464990d0d589e74363e2359d
Ignacio Pulice <ignacio@rsk.co>
2019-07-29 11:13:25 -0300

    Move methods to services, cleanning code

--- src/services/branch-service.ts
+++ src/services/branch-service.ts
@@ -2,24 +2,28 @@ import { MongoStore } from "../storage/mongo-store";
 import Branch from "../common/branch";
 import { ForkDetectionData } from "../common/fork-detection-data";
 
-export class BranchService{
-    
-    private store : MongoStore;
+export class BranchService {
 
-    constructor(store: MongoStore){
+    private store: MongoStore;
+
+    constructor(store: MongoStore) {
         this.store = store;
     }
 
-    public connect() {
+    public connect(): Promise<void> {
+        return this.store.connect();
     }
-    
+
     public disconnect() {
+
     }
 
     public saveBranch(branchToSave: Branch) {
     }
 
-    public getForksDetected(minimunHeightToSearch: number) : ForkDetectionData[] {
-        return [];
+    public async getForksDetected(minimunHeightToSearch: number): Promise<ForkDetectionData[]> {
+
+        let branches: ForkDetectionData[] = await this.store.getCollection().find().toArray();
+        return branches;
     }
 }
--- src/services/rsk-api-service.ts
+++ src/services/rsk-api-service.ts
@@ -1,4 +1,6 @@
 import { RskApiConfig } from "../config/rsk-api-config";
+import { BlockRSK } from "../common/block";
+import { ForkDetectionData } from "../common/fork-detection-data";
 
 export class RskApiService {
    
@@ -8,6 +10,19 @@ export class RskApiService {
 
     connect() {
     }
+
     disconnect() {
+    }
+
+    public getBlocksByHeight(blockNumber: number) {
+        let listBLocks: BlockRSK[] = [];
+
+        listBLocks.push(new BlockRSK(5985954, "hash1rsk", ForkDetectionData.getObject("rskTag1")));
+        listBLocks.push(new BlockRSK(5985954, "hash1rsk", ForkDetectionData.getObject("rskTag2")));
+        listBLocks.push(new BlockRSK(5985954, "hash1rsk", ForkDetectionData.getObject("rskTag3")));
+        listBLocks.push(new BlockRSK(5985954, "hash1rsk", ForkDetectionData.getObject("rskTag4")));
+        listBLocks.push(new BlockRSK(5985954, "hash1rsk", ForkDetectionData.getObject("rskTag5")));
+
+        return listBLocks;
     }
 }
