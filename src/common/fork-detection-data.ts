24a6264fff54614bbcbb5e6aa4cec43b69068a29
Ignacio Pulice <ignacio@rsk.co>
2019-07-31 14:25:57 -0300

    overlap cvp is working and were added test

--- src/common/fork-detection-data.ts
+++ src/common/fork-detection-data.ts
@@ -1,4 +1,4 @@
-import { BtcHeaderInfo } from "./btc-block";
+import { checkTag } from "../util/helper";
 
 export class ForkDetectionData {
     static getObject(tag: string): ForkDetectionData {
@@ -9,22 +9,50 @@ export class ForkDetectionData {
     public CPV: string;
     public NU: number;
     public BN: number;
-   
 
-    constructor(rskTag: string, ) {
-        this.prefixHash = rskTag.substring(0, 20);
-        this.CPV = rskTag.substring(20,27);
-        this.NU = parseInt(rskTag.substring(27,28));
-        this.BN = parseInt(rskTag.substring(28,32));
+    constructor(rskTag: string) {
+        let tag = checkTag(rskTag);
+
+        if(tag != null){
+            tag = tag.substring(2);
+        }
+
+        this.prefixHash = tag.substring(0, 40);
+        this.CPV = tag.substring(40,54);
+        this.NU = parseInt(tag.substring(54,56), 16);
+        this.BN = parseInt(tag.substring(56,64) , 16);
     }
-}
 
-export class BranchItem {
-    public btcInfo: BtcHeaderInfo;
-    public forkDetectionData: ForkDetectionData
+    private hexToBytes(hex) {
+        var bytes = [];
 
-    constructor(btcInfo: BtcHeaderInfo, forkDetectionData: ForkDetectionData){
-        btcInfo  = btcInfo;
-        forkDetectionData = forkDetectionData;
+        for (var c = 0; c < hex.length; c += 2)
+            bytes.push(hex.substr(c, 2));
+
+        return bytes;
+    }
+
+    public overlapCPV(cpvToCheck: string, countCPVtoMatch: number){
+        return this.getNumberOfOverlapInCPV(cpvToCheck) >= countCPVtoMatch;
+    }
+    
+    private getNumberOfOverlapInCPV(cpvToCheck: string): number {
+        let cpvSplit = this.hexToBytes(this.CPV);
+        let cpvToCheckSplit = this.hexToBytes(cpvToCheck);
+        var numberOfMatch = 0;
+
+        for (var i = 0; i < cpvSplit.length && numberOfMatch == 0; i++) {
+            for(var j = 0; j < cpvToCheckSplit.length; j ++){
+                if (cpvSplit[i] == cpvToCheckSplit[j]) {
+                    if(cpvSplit.slice(i).toString() == cpvToCheckSplit.slice(j, cpvToCheckSplit.length - i).toString()){
+                        return cpvSplit.slice(i).length;
+                    };
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return 0;
     }
 }
--- test/test.ts
+++ test/test.ts
@@ -2,21 +2,95 @@ import { expect } from 'chai';
 import 'mocha';
 import { ForkDetectionData } from '../src/common/fork-detection-data';
 
-describe('Hello function', () => {
+describe('For detection tag', () => {
 
-  it('check fork data detection object', () => {
-    expect('Hello world!').to.equal('Hello world!');
+  it('well form with 0x', () => {
+    let rskTag = '0x9bc86e9bfe800d46b85d48f4bc7ca056d2af88a0d89d8bf4d2e43400000004c9'
 
-    let rskTag = '0xb361640849792b80c93c706c2257e821e7e07367b8b897311e8f33263e6af26d'
+    let data: ForkDetectionData = new ForkDetectionData(rskTag);
 
-    let data : ForkDetectionData = new ForkDetectionData(rskTag);
+    expect(data.prefixHash).to.equal('9bc86e9bfe800d46b85d48f4bc7ca056d2af88a0');
+    expect(data.CPV).to.equal('d89d8bf4d2e434');
+    expect(data.NU).to.equal(0);
+    expect(data.BN).to.equal(1225);
 
-    
-    expect(data.BN).to.equal();
-    expect(data.BN).to.equal();
-    expect(data.BN).to.equal();
-    expect(data.BN).to.equal();
+  });
 
+  it('well form', () => {
+    let rskTag = '9bc86e9bfe800d46b85d48f4bc7ca056d2af88a0d89d8bf4d2e43400000004c9'
 
+    let data: ForkDetectionData = new ForkDetectionData(rskTag);
+
+    expect(data.prefixHash).to.equal('9bc86e9bfe800d46b85d48f4bc7ca056d2af88a0');
+    expect(data.CPV).to.equal('d89d8bf4d2e434');
+    expect(data.NU).to.equal(0);
+    expect(data.BN).to.equal(1225);
+  });
+});
+
+describe('Overlap CPV', () => {
+
+  it('cpv match with differents lengh', () => {
+    const prefix = '9bc86e9bfe800d46b85d48f4bc7ca056d2af88a0';
+    const cpv = 'd89d8bf4d2e434'; //["d8", "9d", "8b", "f4", "d2", "e4", "34"]
+    const nu = '00';
+    const bn = '000004c9';
+
+    let forkData = new ForkDetectionData(prefix + cpv + nu + bn);
+
+    //match with "d2", "e4", "34"
+    let cpv1 = 'd89d8bf4d2e434';
+    let overlapped = forkData.overlapCPV(cpv1, 1);
+    expect(overlapped).to.equal(true);
+
+    overlapped = forkData.overlapCPV(cpv1, 2);
+    expect(overlapped).to.equal(true);
+
+    overlapped = forkData.overlapCPV(cpv1, 3);
+    expect(overlapped).to.equal(true);
+
+    overlapped = forkData.overlapCPV(cpv1, 4);
+    expect(overlapped).to.equal(true);
+
+    overlapped = forkData.overlapCPV(cpv1, 5);
+    expect(overlapped).to.equal(true);
+
+    overlapped = forkData.overlapCPV(cpv1, 6);
+    expect(overlapped).to.equal(true);
+
+    overlapped = forkData.overlapCPV(cpv1, 7);
+    expect(overlapped).to.equal(true);
+
+    overlapped = forkData.overlapCPV(cpv1, 8);
+    expect(overlapped).to.equal(false);
+  });
+
+  it('cpv match 3 lengh', () => {
+    const prefix = '9bc86e9bfe800d46b85d48f4bc7ca056d2af88a0';
+    const cpv = 'd89d8bf4d2e434'; //["d8", "9d", "8b", "f4", "d2", "e4", "34"]
+    const nu = '00';
+    const bn = '000004c9';
+
+    let forkData = new ForkDetectionData(prefix + cpv + nu + bn);
+
+    //match with "d2", "e4", "34"
+    let cpv1 = 'd2e43411223344'; //["d2", "e4", "34", "11", "22", "33", "44"]
+    let overlapped = forkData.overlapCPV(cpv1, 3);
+    expect(overlapped).to.equal(true);
+
+    //match with "e4", "34"
+    cpv1 = 'e4341122334455'; //["e4", "34", "11", "22", "33", "44", "55"]
+    overlapped = forkData.overlapCPV(cpv1, 3);
+    expect(overlapped).to.equal(false);
+
+    //match with "34"
+    cpv1 = '34112233445566'; //["34", "11", "22", "33", "44", "55", "66"]
+    overlapped = forkData.overlapCPV(cpv1, 3);
+    expect(overlapped).to.equal(false);
+
+    //doesn't match match anything
+    cpv1 = '34112233445566'; //["11", "22", "33", "44", "55", "66", "77"]
+    overlapped = forkData.overlapCPV(cpv1, 3);
+    expect(overlapped).to.equal(false);
   });
 });
