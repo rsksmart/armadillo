ea3c7f1d591bd25005b5d54e21dffe918d5fc852
Martin Picco <mpicco@iovlabs.org>
2019-08-01 16:28:38 -0300

    Tidy up imports

--- test/test.ts
+++ test/test.ts
@@ -1,15 +1,12 @@
 import "mocha";
 import { BtcHeaderInfo } from "../src/common/btc-block";
-import { BtcMonitor } from "../src/services/btc-monitor";
+import { BtcWatcher } from "../src/services/btc-watcher";
 import { Branch, BranchItem } from "../src/common/branch";
 import { expect } from "chai";
 import { ForkDetector } from "../src/services/fork-detector";
 import { ForkDetectionData } from "../src/common/fork-detection-data";
 import { stubObject } from "ts-sinon";
 import sinon from "sinon";
-import { MainConfig } from "../src/config/main-config";
-import { MongoStore } from "../src/storage/mongo-store";
-import { BranchService } from "../src/services/branch-service";
 
 const PREFIX = "9bc86e9bfe800d46b85d48f4bc7ca056d2af88a0";
 const CPV = "d89d8bf4d2e434"; // ["d8", "9d", "8b", "f4", "d2", "e4", "34"]
@@ -107,7 +104,7 @@ describe("Overlap CPV", () => {
 
   it("getBranchesThatOverlap return 1", async () => {
     const forkData = new ForkDetectionData(RSKTAG);
-    const btcStub = stubObject<BtcMonitor>(BtcMonitor.prototype);
+    const btcStub = stubObject<BtcWatcher>(BtcWatcher.prototype);
     const btcInfo = stubObject<BtcHeaderInfo>(BtcHeaderInfo.prototype);
     const forkDetector = new ForkDetector(null, btcStub, null);
 
@@ -125,7 +122,7 @@ describe("Overlap CPV", () => {
   it("getBranchesThatOverlap return 2 elements", async () => {
 
     const forkData = new ForkDetectionData(RSKTAG);
-    const btcStub = stubObject<BtcMonitor>(BtcMonitor.prototype);
+    const btcStub = stubObject<BtcWatcher>(BtcWatcher.prototype);
     const btcInfo = stubObject<BtcHeaderInfo>(BtcHeaderInfo.prototype);
     const forkDetector = new ForkDetector(null, btcStub, null);
     const forkData1 = new ForkDetectionData(RSKTAG1);
