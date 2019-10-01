e04a5327920efbed4e1b8d22b696a78f18332fa9
Ignacio <ignacio@iovlabs.org>
2019-10-01 11:32:42 -0300

    Forgot to add some changes to new btc service

--- src/services/mainchain-service.ts
+++ src/services/mainchain-service.ts
@@ -1,19 +1,12 @@
 import { MongoStore } from "../storage/mongo-store";
 import { BranchItem } from "../common/branch";
 import BaseService from "./base-service";
+import { RskBlock } from "../common/rsk-block";
 
 export class MainchainService  extends BaseService {
 
      constructor(store: MongoStore) {
        super(store);
-    }
-
-    public connect(): Promise<void> {
-        return this.store.connect();
-    }
-
-    public disconnect() {
-        this.store.disconnect();
     }
 
     public async getLastItems(numberOfItems): Promise<BranchItem[]> {
