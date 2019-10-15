1cace95e542dbc67a793c7c40cc6429e9338ba61
edgardo <edgardo@iovlabs.org>
2019-10-15 11:52:52 -0300

    In case of not having btc header information return null

--- src/common/btc-block.ts
+++ src/common/btc-block.ts
@@ -1,29 +1,32 @@
 import { ForkDetectionData } from "./fork-detection-data";
 
 export class BtcBlock {
-    public btcInfo: BtcHeaderInfo;
-    public rskTag: ForkDetectionData;
+  public btcInfo: BtcHeaderInfo;
+  public rskTag: ForkDetectionData;
 
-    constructor(_height: number, _hash: string, _rskTag: string) {
-        this.btcInfo = new BtcHeaderInfo(_height, _hash);
-        
-        if (_rskTag && _rskTag.length > 0) {
-            this.rskTag = new ForkDetectionData(_rskTag);
-        }
+  constructor(_height: number, _hash: string, _rskTag: string) {
+    this.btcInfo = new BtcHeaderInfo(_height, _hash);
+
+    if (_rskTag && _rskTag.length > 0) {
+      this.rskTag = new ForkDetectionData(_rskTag);
     }
+  }
 }
 
 export class BtcHeaderInfo {
-    public height?: number;
-    public hash?: string;
+  public height?: number;
+  public hash?: string;
 
-    constructor(_height?: number, _hash?: string) {
-        this.height = _height;
-        this.hash = _hash;
-    }
+  constructor(_height?: number, _hash?: string) {
+    this.height = _height;
+    this.hash = _hash;
+  }
 
-    public static fromObject(btcInfo: any): BtcHeaderInfo {
-        return new BtcHeaderInfo(btcInfo.height, btcInfo.hash);
+  public static fromObject(btcInfo: any): BtcHeaderInfo {
+    if (btcInfo != null) {
+      return new BtcHeaderInfo(btcInfo.height, btcInfo.hash);
+    } else {
+      return null;
     }
+  }
 }
-
