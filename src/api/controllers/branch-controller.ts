5a396b4fd3998efa0278692f5096d4cd86df803b
Ignacio <ignacio@iovlabs.org>
2019-10-23 13:16:43 -0300

    Error handling

--- src/api/controllers/branch-controller.ts
+++ src/api/controllers/branch-controller.ts
@@ -12,16 +12,21 @@ export default class BranchController {
     // this.service.createIndex({ "header.height": 1 }, { unique: false });
   }
 
-  public async getForksDetected(req: any, res: any): Promise<MessageResponse<Branch[]>> {
-    const n: number = parseInt(req.params.n);
-    var forks: Branch[] = await this.service.getForksDetected(n);
+  public async getForksDetected(req: any, res: any, next: any): Promise<MessageResponse<Branch[]>> {
+    try {
+      
+      const n: number = parseInt(req.params.n);
+      var forks: Branch[] = await this.service.getForksDetected(n);
+      return res.status(200).send(
+        new MessageResponse<Branch[]>(
+          `Get last forks detected in the last ${n} blocks`,
+           true,
+            forks
+        )
+      );
 
-    return res.status(200).send(
-      new MessageResponse<Branch[]>(
-        `Get last forks detected in the last ${n} blocks`,
-        true,
-        forks
-      )
-    );
+    } catch (error) {
+      next(error);
+    }
   }
 }
