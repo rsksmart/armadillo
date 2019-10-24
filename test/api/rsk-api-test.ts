80f5a2e4ff7bff3893839fc8dcb28ff67d0460cf
Ignacio <ignacio@iovlabs.org>
2019-10-24 01:23:05 -0300

    Added new field mainchain into Rskblock
    Now when tag which is not in mainnet (is an uncle) is found, we just save best block into mainchain, and drop btc block with rsk tag found (this changes is for first version)

--- test/api/rsk-api-test.ts
+++ test/api/rsk-api-test.ts
@@ -26,7 +26,7 @@ const RskTagMatch5 = PREFIX + CPVMatch5 + NU + BN;
 const RskTagMatch4 = PREFIX + CPVMatch4 + NU + BN;
 const RSKTagMatch6 = PREFIX + CPVMatch6 + NU + BN;
 const RskTagMatch7 = PREFIX + CPVMatch7 + NU + BN;
-const rskBlock = new RskBlock(1000, "", "", new ForkDetectionData(RskTagMatch7));
+const rskBlock = new RskBlock(1000, "", "", true, new ForkDetectionData(RskTagMatch7));
 let rskApiConfig: RskApiConfig;
 let rskApiService: RskApiService;
 
@@ -39,9 +39,9 @@ describe("Rsk Service api tests", () => {
 
   it("getRskBlockAtCertainHeight method, match in 0 bytes the CPV, then get Block from rsk that connect fork with mainchain", async () => {
 
-    let blockSameHeightMatch3 = new RskBlock(1000, "", "", new ForkDetectionData(RskTagMatch0));
-    let blockHeight1 = new RskBlock(1, "", "", new ForkDetectionData(RskTagMatch7));
-    let bestBlock = new RskBlock(10, "", "", new ForkDetectionData(RskTagMatch7));
+    let blockSameHeightMatch3 = new RskBlock(1000, "", "", true, new ForkDetectionData(RskTagMatch0));
+    let blockHeight1 = new RskBlock(1, "", "", true, new ForkDetectionData(RskTagMatch7));
+    let bestBlock = new RskBlock(10, "", "", true, new ForkDetectionData(RskTagMatch7));
     var getBlockAtHeight1 = sinon.stub(rskApiService, <any>'getBlock');
     getBlockAtHeight1.withArgs(1).returns(blockHeight1);
     var getBestBlock = sinon.stub(rskApiService, <any>'getBestBlock');
@@ -55,9 +55,9 @@ describe("Rsk Service api tests", () => {
 
   it("getRskBlockAtCertainHeight method, match in 3 bytes the CPV, then get Block from rsk that connect fork with mainchain", async () => {
 
-    let blockSameHeightDontMatch = new RskBlock(1000, "", "", new ForkDetectionData(RskTagMatch3));
-    let block768 = new RskBlock(768, "", "", new ForkDetectionData(RskTagMatch2));
-    let block832 = new RskBlock(832, "", "", new ForkDetectionData(RskTagMatch2));
+    let blockSameHeightDontMatch = new RskBlock(1000, "", "", true, new ForkDetectionData(RskTagMatch3));
+    let block768 = new RskBlock(768, "", "", true, new ForkDetectionData(RskTagMatch2));
+    let block832 = new RskBlock(832, "", "", true, new ForkDetectionData(RskTagMatch2));
     var getBlock = sinon.stub(rskApiService, <any>'getBlock');
     getBlock.withArgs(768).returns(block768);
     getBlock.withArgs(832).returns(block832);
@@ -70,9 +70,9 @@ describe("Rsk Service api tests", () => {
 
   it("getRskBlockAtCertainHeight method, match in 5 bytes the CPV, then get Block from rsk that connect fork with mainchain", async () => {
 
-    let blockSameHeightDontMatch = new RskBlock(1000, "", "", new ForkDetectionData(RskTagMatch5));
-    let block832 = new RskBlock(832, "", "", new ForkDetectionData(RskTagMatch5));
-    let block896 = new RskBlock(896, "", "", new ForkDetectionData(RskTagMatch5));
+    let blockSameHeightDontMatch = new RskBlock(1000, "", "", true, new ForkDetectionData(RskTagMatch5));
+    let block832 = new RskBlock(832, "", "", true, new ForkDetectionData(RskTagMatch5));
+    let block896 = new RskBlock(896, "", "", true, new ForkDetectionData(RskTagMatch5));
     var getBlock = sinon.stub(rskApiService, <any>'getBlock');
     getBlock.withArgs(832).returns(block832);
     getBlock.withArgs(896).returns(block896);
@@ -86,8 +86,8 @@ describe("Rsk Service api tests", () => {
 
   it("getRskBlockAtCertainHeight method, match in 7 bytes the CPV, then get Block from rsk that connect fork with mainchain", async () => {
 
-    let block960 = new RskBlock(960, "", "", new ForkDetectionData(RskTagMatch7));
-    let block1000 = new RskBlock(1000, "", "", new ForkDetectionData(RskTagMatch7));
+    let block960 = new RskBlock(960, "", "", true, new ForkDetectionData(RskTagMatch7));
+    let block1000 = new RskBlock(1000, "", "", true, new ForkDetectionData(RskTagMatch7));
     var getBlock = sinon.stub(rskApiService, <any>'getBlock');
     getBlock.withArgs(960).returns(block960);
     getBlock.withArgs(1000).returns(block1000);
