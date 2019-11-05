be1d6ec18c0d40f6192c057fc6eb71640299f1b9
edgardo <edgardo@iovlabs.org>
2019-11-05 10:30:22 -0300

    save function try catch block

--- src/services/mainchain-service.ts
+++ src/services/mainchain-service.ts
@@ -2,49 +2,65 @@ import { MongoStore } from "../storage/mongo-store";
 import { BranchItem } from "../common/branch";
 import BaseService from "./base-service";
 
-export class MainchainService  extends BaseService {
+export class MainchainService extends BaseService {
+  constructor(store: MongoStore) {
+    super(store);
+  }
 
-     constructor(store: MongoStore) {
-       super(store);
+  public async getLastItems(numberOfItems): Promise<BranchItem[]> {
+    let robjectsToReturn: any[] = await this.store
+      .getCollection()
+      .find()
+      .sort({ "rskInfo.height": -1 })
+      .limit(numberOfItems)
+      .toArray();
+    return robjectsToReturn.map(x => BranchItem.fromObject(x));
+  }
+
+  public async getBestBlock(): Promise<BranchItem> {
+    let items: BranchItem[] = await this.getLastItems(1);
+    return items.length > 0 ? items[0] : null;
+  }
+
+  public async save(items: BranchItem[]): Promise<boolean> {
+    var response = true;
+    try {
+      await this.store.getCollection().insertMany(items);
+    } catch (ex) {
+      response = false;
     }
+    return response;
+  }
 
-    public async getLastItems(numberOfItems): Promise<BranchItem[]> {
-        let robjectsToReturn : any[] = await this.store.getCollection().find().sort({ "rskInfo.height": -1 }).limit(numberOfItems).toArray();
-        return robjectsToReturn.map(x => BranchItem.fromObject(x));
-    }
+  public async removeLastBlocks(n: number): Promise<string[]> {
+    const removeIdsArray: any[] = await this.store
+      .getCollection()
+      .find()
+      .limit(n)
+      .sort({ "rskInfo.height": -1 })
+      .toArray()
+      .then(list =>
+        list.map(function(doc) {
+          return doc._id;
+        })
+      );
 
-    public async getBestBlock(): Promise<BranchItem> {
-       let items : BranchItem[] = await this.getLastItems(1);
-       return items.length > 0 ? items[0] : null;
-    }
+    await this.store
+      .getCollection()
+      .deleteMany({ _id: { $in: removeIdsArray } });
 
-    public async save(items: BranchItem[]): Promise<boolean> {
-        var response = true;
+    return removeIdsArray;
+  }
 
-        await this.store.getCollection().insertMany(items).catch(function(ex){
-            response = false;
-        });
+  public async getAll(): Promise<BranchItem[]> {
+    let items: any[] = await this.store
+      .getCollection()
+      .find({})
+      .toArray();
+    return items.map(x => BranchItem.fromObject(x));
+  }
 
-        return response;
-    }
-    
-    public async removeLastBlocks(n: number) : Promise<string[]> {
-        const removeIdsArray: any[] = await this.store.getCollection().find()
-        .limit(n)
-        .sort({ "rskInfo.height" : -1 })
-        .toArray().then(list => list.map(function (doc) { return doc._id; }));
-    
-        await this.store.getCollection().deleteMany({ _id: { $in: removeIdsArray }})
-       
-        return removeIdsArray;
-    }
-
-    public async getAll() : Promise<BranchItem[]>{
-        let items : any[] = await this.store.getCollection().find({}).toArray();
-        return items.map(x => BranchItem.fromObject(x));
-    }
-
-    public async deleteAll(){
-        await this.store.getCollection().drop();
-    }
+  public async deleteAll() {
+    await this.store.getCollection().drop();
+  }
 }
