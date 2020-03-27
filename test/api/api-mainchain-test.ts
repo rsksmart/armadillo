bff8679281dd5642b0acfb92ac7c6e92feee4bb3
Ignacio <ignacio@iovlabs.org>
2020-03-27 16:01:29 -0300

    Added endpoint to calculate hashing power

--- test/api/api-mainchain-test.ts
+++ test/api/api-mainchain-test.ts
@@ -151,7 +151,6 @@ describe("Mainchain api tests", () => {
     expect(itemToChange).to.deep.equal(itemSaved);
   });
 
-
   it("getLastBtcBlocksDetectedInChain method", async () => {
     // Add some items into armadillo chain and then check if the given chain is correct
     let item1WithBTC = new Item(new BtcHeaderInfo(1000, "", ""), new RskBlockInfo(100, "hash", "prevHash", true, "", new ForkDetectionData(RSKTAG)));
@@ -217,5 +216,106 @@ describe("Mainchain api tests", () => {
     response = await mainchainService.getLastBtcBlocksDetectedInChainCompleteWithRSK(2);
     expect(response.length).to.equal(11);
     expect([item3WithBTC].concat(itemsToSaveBetween3and4).reverse()).to.deep.equal(response);
+  });
+
+  it("getBtcBlocksBetweenHeight method", async () => {
+    // Add some items into armadillo chain and then check if the given chain is correct
+    const item3 = new Item(new BtcHeaderInfo(3000, "", ""), new RskBlockInfo(300, "hash", "prevHash", true, "", new ForkDetectionData(RSKTAG)));
+    const item2 = new Item(new BtcHeaderInfo(2000, "", ""), new RskBlockInfo(200, "hash", "prevHash", true, "", new ForkDetectionData(RSKTAG)));
+    const item1 = new Item(new BtcHeaderInfo(1000, "", ""), new RskBlockInfo(100, "hash", "prevHash", true, "", new ForkDetectionData(RSKTAG)));
+
+    // start is bigger than end return []
+    let response : Item[] = await mainchainService.getBtcBlocksBetweenHeight(1000, 200);
+    expect(response.length).to.equal(0);
+
+    // no btc blocks with interval 100 - 200
+    await mainchainService.save([copy(item1)]);
+    response = await mainchainService.getBtcBlocksBetweenHeight(100, 200);
+    expect(response.length).to.equal(0);
+
+    // 1 item with interval 1000 - 2000
+    response = await mainchainService.getBtcBlocksBetweenHeight(1000, 2000);
+    expect([item1]).to.deep.equal(response);
+
+    // 2 items with interval 1000 - 2000
+    await mainchainService.save([copy(item2)]);
+    await mainchainService.save([copy(item3)]);
+    response = await mainchainService.getBtcBlocksBetweenHeight(1000, 2000);
+    expect([item2, item1]).to.deep.equal(response);
+
+    // 2 items with interval 2000 - 3000
+    response = await mainchainService.getBtcBlocksBetweenHeight(2000, 3000);
+    expect([item3, item2]).to.deep.equal(response);
+
+    // 2 items with interval 3000 - 3000
+    response = await mainchainService.getBtcBlocksBetweenHeight(3000, 3000);
+    expect([item3]).to.deep.equal(response);
+
+    // add rsk blocks without btc data should be in the response
+    const itemRsk = new Item(null, new RskBlockInfo(102, "hash", "prevHash", true, "", new ForkDetectionData(RSKTAG)));
+    await mainchainService.save([copy(itemRsk)]);
+    response = await mainchainService.getBtcBlocksBetweenHeight(100, 1000);
+    expect([item1]).to.deep.equal(response);
+  });
+
+  it("getBtcBlocksBetweenRskHeight method", async () => {
+    // Add some items into armadillo chain and then check if the given chain is correct
+    const item3 = new Item(new BtcHeaderInfo(3000, "", ""), new RskBlockInfo(300, "hash", "prevHash", true, "", new ForkDetectionData(RSKTAG)));
+    const item2 = new Item(new BtcHeaderInfo(2000, "", ""), new RskBlockInfo(200, "hash", "prevHash", true, "", new ForkDetectionData(RSKTAG)));
+    const item1 = new Item(new BtcHeaderInfo(1000, "", ""), new RskBlockInfo(100, "hash", "prevHash", true, "", new ForkDetectionData(RSKTAG)));
+
+    // start is bigger than end return []
+    let response : Item[] = await mainchainService.getBtcBlocksBetweenRskHeight(1000, 200);
+    expect(response.length).to.equal(0);
+
+    // no btc blocks with interval 1000 - 2000
+    await mainchainService.save([copy(item1)]);
+    response = await mainchainService.getBtcBlocksBetweenRskHeight(1000, 2000);
+    expect(response.length).to.equal(0);
+
+    // 1 item with interval 1000 - 2000
+    response = await mainchainService.getBtcBlocksBetweenRskHeight(100, 150);
+    expect([item1]).to.deep.equal(response);
+
+    // 2 items with interval 1000 - 2000
+    await mainchainService.save([copy(item2)]);
+    await mainchainService.save([copy(item3)]);
+    response = await mainchainService.getBtcBlocksBetweenRskHeight(100, 200);
+    expect([item2, item1]).to.deep.equal(response);
+
+    // 2 items with interval 2000 - 3000
+    response = await mainchainService.getBtcBlocksBetweenRskHeight(200, 300);
+    expect([item3, item2]).to.deep.equal(response);
+
+    // 2 items with interval 3000 - 3000
+    response = await mainchainService.getBtcBlocksBetweenRskHeight(300, 300);
+    expect([item3]).to.deep.equal(response);
+
+    // add rsk blocks without btc data should be in the response
+    const itemRsk = new Item(null, new RskBlockInfo(102, "hash", "prevHash", true, "", new ForkDetectionData(RSKTAG)));
+    await mainchainService.save([copy(itemRsk)]);
+    response = await mainchainService.getBtcBlocksBetweenRskHeight(100, 200);
+    expect([item2, item1]).to.deep.equal(response);
+  });
+
+  it("getBtcBlocksBetweenRskHeight and getBtcBlocksBetweenHeight method from route", async () => {
+    const btcInfo1 = new BtcHeaderInfo(1000, "", "");
+    const btcInfo2 = new BtcHeaderInfo(2000, "", "");
+    const item = new Item(btcInfo1, new RskBlockInfo(100, "hash", "prevHash", true, "", new ForkDetectionData(RSKTAG)));
+    const item2 = new Item(btcInfo2, new RskBlockInfo(200, "hash", "prevHash", true, "", new ForkDetectionData(RSKTAG)));
+
+    await mainchainService.save([copy(item)]);
+    await mainchainService.save([copy(item2)]);
+
+    const mainchainController = new MainchainController(mainchainService);
+    let param = { "params": { "startHeight": 100, "endHeight": 150} };
+    let response : MessageResponse<Item[]> = await mainchainController.getBtcBlocksBetweenRskHeight(param, mockRes);
+    expect(response.data.length).to.equal(1);
+
+    //startHeight > endHeight must return 
+    param = { "params": { "startHeight": 200, "endHeight": 150} };
+    response = await mainchainController.getBtcBlocksBetweenRskHeight(param, mockRes);
+    expect(response.data.length).to.equal(0);
+    expect(response.success).to.equal(false);
   });
 });
