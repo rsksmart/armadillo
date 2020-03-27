c955d4d556ae641718a422765f439e9d959447a1
mpicco <mpicco@iovlabs.org>
2020-03-27 18:58:14 -0300

    Extract ForkEmailBuilder interface/class

--- src/scripts/fork-detector-notifier/src/common/model.ts
+++ src/scripts/fork-detector-notifier/src/common/model.ts
@@ -1,24 +1,4 @@
-import { RangeForkInMainchain, Fork } from "../../../../common/forks";
-
-export class ForkInformationEmail {
-    public btcGuessedMinersNames: string[];
-    public forkBTCitemsLength: number;
-    public forkTime: string;
-    public distanceFirstItemToBestBlock: number;
-    public cpvInfo: any;
-    public distanceCPVtoPrevJump: number;
-    public bestBlockInRskInThatMoment: number;
-    public rangeWhereForkCouldHaveStarted: RangeForkInMainchain;
-    public chainDistance: any;
-    public btcListHeights: number[];
-    public forkLengthRskBlocks: number;
-    public btcGuessedMinedInfo: GuessMinedBlockInfo[];
-    public fork: Fork
+export interface ForkEmail {
+    subject: string;
+    body: string;
 }
-
-export class GuessMinedBlockInfo {
-  public poolName: string;
-  public totalPorcentageOfBlocksMined: number;
-  public numberOfBlocksMined: number;
-}
-  
