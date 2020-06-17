9317f660db0e0e1a439900e1d720e149b6c4ccf4
Ignacio <ignacio@iovlabs.org>
2020-06-17 01:20:41 -0300

    Added new tests to validate notifications flow storage

--- src/scripts/fork-detector-notifier/src/common/notification-service.ts
+++ src/scripts/fork-detector-notifier/src/common/notification-service.ts
@@ -9,7 +9,6 @@ export class NotificationService extends BaseService {
     }
 
     public async notificationForkWasSent(identifier: string): Promise<boolean> {
-        //First remove the existing item and then save the new items
         var count = await this.store.getCollection().countDocuments({ "identifier": identifier }, { limit: 1 });
         return count > 0;
     }
